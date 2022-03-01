7380748fcbfdb2e20370005d72f3fbfffb581e3b
kirstytheproud <k.joproud@gmail.com>
2022-03-01 15:29:14 +0000

    basic layout

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("input");
+const addButton = document.getElementById("add-to-list");
+
+// const getNameInput = (event) => {
+//     const newName = input.value;
+//     console.log(newName);
+//     document.querySelector(".title").innerHTML = "Welcome Back " + newName;
+
+// }
+// console.log("its working")
+
+// input.addEventListener("input", getNameInput);
+
+// input.addEventListener("keyup", function(event) {
+//   if (e === 13) {
+//    event.preventDefault();
+//    input.value = " ";
+//   }
+// });
